ea18865280685ff578359c257ba4c4150bcfe25d
aroo-dev <axxrxxo@gmail.com>
2024-05-21 17:50:55 +0200

    init

--- src/main/java/com/example/demo/YearOfEmployment.java
+++ src/main/java/com/example/demo/YearOfEmployment.java
@@ -1,4 +1,19 @@
 package com.example.demo;
 
 public class YearOfEmployment {
+    private int year;
+
+
+    public YearOfEmployment(int year) {
+        this.year = year;
+    }
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
